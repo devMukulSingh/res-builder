8cb70537e10fe2ad6179d1c39721fdd1795b86b8
JOHN CARTER <mukulsingh2276@gmail.com>
2024-03-12 17:19:44 +0530

    fix in language Page

--- app/(root)/builder/[templateId]/(routes)/language/page.tsx
+++ app/(root)/builder/[templateId]/(routes)/language/page.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic'
-const LanguageForm = dynamic( () => import('./components/LanguageForm'),{ssr:falsew}  )
+const LanguageForm = dynamic( () => import('./components/LanguageForm'),{ssr:false}  )
 
 const Language = () => {
   return (
